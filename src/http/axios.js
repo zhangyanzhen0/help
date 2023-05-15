a80649f96d11df5a1610e45fab362c2c8d8f34d3
wuzongbo_9688 <wuzongbo_9688@126.com>
2023-05-15 14:57:06 +0800

    axios

--- src/http/axios.js
+++ src/http/axios.js
@@ -4,7 +4,7 @@ import {showLoading,hideLoading } from '@/utils/eloading'
 
 //创建axios实例
 const instance=axios.create({
-    baseURL:'http://api.jqrjq.cn/',
+    baseURL:'http://mall.qzimp.cn/',
     timeout:6000
 })
 //请求拦截器
