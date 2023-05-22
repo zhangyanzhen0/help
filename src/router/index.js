70e0c685882cd7ca8088c51c7df685dca9b7cdf5
林淑真 <1940790110@qq.com>
2023-05-22 05:40:28 +0000

    2003010158 林淑真
    Signed-off-by: 林淑真 <1940790110@qq.com>

--- src/router/index.js
+++ src/router/index.js
@@ -34,6 +34,11 @@ const routes = [
     component: () => import('@/views/admin/pms_feight_template.vue'),
   },
   {
+    path: '/sms_flash_promotion', //访问路径 http://127.0.0.1:5173/cx/XfDGdOkjVo7deIxQjurcf8BEnNf
+    name: 'sms_flash_promotion', //命名路由
+    component: () => import('@/views/admin/sms_flash_promotion.vue'),
+  },
+  {
     path: '/ums_member_tag', //访问路径 http://127.0.0.1:5173/cx/XfDGdOkjVo7deIxQjurcf8BEnNf
     name: 'ums_member_tag', //命名路由
     component: () => import('@/views/admin/ums_member_tag.vue'),
