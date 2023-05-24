49bfb608449efb17b45683c6a1a46d9dbb92a420
YQL <2644707621@qq.com>
2023-05-24 18:20:04 +0800

    2003010149 杨秋霖

--- src/router/index.js
+++ src/router/index.js
@@ -42,6 +42,11 @@ const routes = [
     name: 'ums_member_task', //命名路由
     component: () => import('@/views/admin/ums_member_task.vue'),
   },
+              {
+                path: '/oms-order-setting',
+                name: 'oms-order-setting',
+                component: () => import('@/views/admin/oms-order-setting.vue')
+            },
   {
     path: '/cms_help_category', //访问路径 http://127.0.0.1:5173/cx/XfDGdOkjVo7deIxQjurcf8BEnNf
     name: 'cms_help_category', //命名路由
